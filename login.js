aaad7cef9dc39692bf9a8119363ed3ea7623e6a4
Ichson <xson2582002@gmail.com>
2024-07-18 08:48:41 +0700

    responsive by css

---
+++ login.js
@@ -0,0 +1,38 @@
+const formInput = document.getElementById('formInput');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const errMessageEmail = document.getElementById('errMessageEmail');
+const errMessagePassword = document.getElementById('errMessagePassword');
+const alert1 = document.getElementById('alert');
+
+formInput.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!email.value || !password.value) {
+        if (!email.value) {
+            email.style.border = '1px red solid';
+            errMessageEmail.style.display = ''
+        }
+        else {
+            email.style.border = '1px solid rgba(141, 153, 187, 1)';
+            errMessageEmail.style.display = 'none'
+        }
+        if (!password.value) {
+            password.style.border = '1px red solid';
+            errMessagePassword.style.display = '';
+        }
+        else {
+            password.style.border = '1px solid rgba(141, 153, 187, 1)';
+            errMessagePassword.style.display = 'none';
+        }
+    }
+    else {
+        if (email.value !== 'admin@gmail.com' || password.value !== '123456') {
+            alert1.style.display = '';
+            setTimeout(() => {
+                document.getElementById('alert').style.display = 'none';
+            }, 3000);
+        }
+    }
+
+});
+
